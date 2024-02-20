d8ee76dce8b585d319f1ee9e0e07915302030543
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-02-20 08:30:32 +0530

    set up for the netflixgpt

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-2xl font-bold text-red-500 ">
+      Namaste EveryOne, let's build netflix gpt
+    </div>
+  );
+}
+
+export default App;
