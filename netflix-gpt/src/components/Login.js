19864c73ddf83bc09525c80603f1cef764dee611
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-02-20 10:12:10 +0530

    create signin and sign up page using only one component

--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from "react";
+import Header from "./Header";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [isSignInForm, setIsSignInForm] = useState(true);
 
-export default Login
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877f-45ba7a60c16f/2642e08e-4202-490e-8e93-aff04881ee8a/IN-en-20240212-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="bg"
+        />
+      </div>
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="text-3xl font-bold py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full rounded-md bg-gray-800"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full rounded-md bg-gray-800"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full rounded-md bg-gray-800"
+        />
+        <button className="p-4 my-6 bg-red-600 w-full rounded-md">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already Registered.Sign In Now"}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
