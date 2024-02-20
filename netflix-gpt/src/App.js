29076ef17af378615de80d6630dc1d2bb90521b1
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-02-20 09:03:27 +0530

    set up the project with creacting components body browse header login

--- netflix-gpt/src/App.js
+++ netflix-gpt/src/App.js
@@ -1,11 +1,7 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="text-2xl font-bold text-red-500 ">
-      Namaste EveryOne, let's build netflix gpt
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
