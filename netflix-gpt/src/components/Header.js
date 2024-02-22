7ce91e41d94c5cd42e156b8599a1d8b44eac7436
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-02-22 09:20:24 +0530

    api

--- netflix-gpt/src/components/Header.js
+++ netflix-gpt/src/components/Header.js
@@ -43,11 +43,7 @@ const Header = () => {
 
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img
-        className="w-44"
-        src={LOGO}
-        alt="logo"
-      />
+      <img className="w-44" src={LOGO} alt="logo" />
       {user && (
         <div className="flex p-2 gap-2">
           <img className="w-12 h-12" src={user?.photoURL} alt="userIcon" />
--- netflix-gpt/src/utils/constants.js
+++ netflix-gpt/src/utils/constants.js
@@ -3,3 +3,17 @@ export const LOGO =
 
 export const USER_AVATAR =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe8eIW8UaYc7fD5QyVa_Z39U07KJzGel20cRbqsURLvQ&s";
+
+// const API = "2f3d0ac9fcb8af50179cee049c93198b";
+
+// const API_ACCESS_TOKEN =
+//   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZjNkMGFjOWZjYjhhZjUwMTc5Y2VlMDQ5YzkzMTk4YiIsInN1YiI6IjY1ZDVlZjk0ZGIxNTRmMDE3ZGEwZTgwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._UoI_a8zVThR0sx0-I1pBTsPzpIBHFxutD04eAZyz1o";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZjNkMGFjOWZjYjhhZjUwMTc5Y2VlMDQ5YzkzMTk4YiIsInN1YiI6IjY1ZDVlZjk0ZGIxNTRmMDE3ZGEwZTgwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._UoI_a8zVThR0sx0-I1pBTsPzpIBHFxutD04eAZyz1o",
+  },
+};
