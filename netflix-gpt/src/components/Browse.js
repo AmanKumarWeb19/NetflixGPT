bbf4824d66edd6a36414be4325ac55b7bd3feaec
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-02-22 09:43:35 +0530

    create a custom hook for nowplaying movies

--- netflix-gpt/src/components/Browse.js
+++ netflix-gpt/src/components/Browse.js
@@ -1,20 +1,8 @@
-import React, { useEffect } from "react";
+import useNowPlayingMovies from "../Hooks/useNowPlayingMovies";
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/constants";
 
 const Browse = () => {
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json.results);
-  };
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
+  useNowPlayingMovies(); //custom hooks creating in hooks folder
   return (
     <div>
       <Header />
--- netflix-gpt/src/utils/appStore.js
+++ netflix-gpt/src/utils/appStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./useSlice";
+import moviesReducer from "./moviesSlice";
 
 const appStore = configureStore({
   reducer: {
     user: userReducer,
+    movies: moviesReducer,
   },
 });
 
