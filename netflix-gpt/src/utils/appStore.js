1a2aa4a6c51b3b0e7e2ecb306b3a817e95b91bff
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-02-23 09:59:33 +0530

    add configreducer to my appStore

--- netflix-gpt/src/utils/appStore.js
+++ netflix-gpt/src/utils/appStore.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./useSlice";
 import moviesReducer from "./moviesSlice";
 import gptReducer from "./gptSlice";
+import configReducer from "./langConfigSlice";
 
 const appStore = configureStore({
   reducer: {
     user: userReducer,
     movies: moviesReducer,
     gpt: gptReducer,
+    config: configReducer,
   },
 });
 
